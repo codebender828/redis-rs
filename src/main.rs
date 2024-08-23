9d1436c25d58c2f2ced8f6878c0b6bc41197f3f9
codebender828 <codebender828@gmail.com>
2024-08-23 17:54:57 +0300

    feat: implement concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,35 @@
-// Uncomment this block to pass the first stage
-use std::{
-    io::{Read, Write},
-    net::{TcpListener, TcpStream},
-};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[tokio::main]
+async fn main() {
+    println!("Starting Redis Server!");
 
-    // Uncomment this block to pass the first stage
-    //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
+    loop {
+        let stream = listener.accept().await;
+
         match stream {
-            Ok(stream) => handle_connection(stream),
+            Ok((stream, _)) => handle_connection(stream),
             Err(e) => {
                 println!("error: {}", e);
             }
-        }
+        };
     }
 }
 
+/** Handles TCP connections to Redis Server */
 fn handle_connection(mut stream: TcpStream) {
     println!("accepted new connection");
-    let mut buf = [0; 512];
-
-    loop {
-        let read_count = stream.read(&mut buf).unwrap();
-        if read_count == 0 {
-            break;
+    tokio::spawn(async move {
+        loop {
+            let mut buf = [0; 512];
+            let read_count = stream.read(&mut buf).await.unwrap();
+            if read_count == 0 {
+                break;
+            }
+            stream.write(b"+PONG\r\n").await.unwrap();
         }
-
-        stream.write(b"+PONG\r\n").unwrap();
-    }
+    });
 }
